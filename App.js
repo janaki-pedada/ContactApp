0216d422db124ec5c8115dd62a8a06ba032b1267
janaki-pedada <137858245+janaki-pedada@users.noreply.github.com>
2023-07-29 12:43:22 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,46 +1,10 @@
-import React, { useState } from 'react';
-import ContactForm from './components/ContactForm';
-import ContactList from './components/ContactList';
+import React from 'react';
+import ContactForm from './ContactForm';
 
 const App = () => {
-  const [contacts, setContacts] = useState([]);
-  const [editingContact, setEditingContact] = useState(null);
-
-  const handleCreateContact = (newContact) => {
-    setContacts([...contacts, newContact]);
-  };
-
-  const handleUpdateContact = (updatedContact) => {
-    const updatedContacts = contacts.map((contact) =>
-      contact.id === updatedContact.id ? updatedContact : contact
-    );
-    setContacts(updatedContacts);
-    setEditingContact(null);
-  };
-
-  const handleDeleteContact = (contactToDelete) => {
-    const filteredContacts = contacts.filter((contact) => contact.id !== contactToDelete.id);
-    setContacts(filteredContacts);
-    setEditingContact(null);
-  };
-
-  const handleEditContact = (contactToEdit) => {
-    setEditingContact(contactToEdit);
-  };
-
   return (
     <div>
-      <h1>Contact Manager</h1>
-      <ContactForm
-        onCreateContact={handleCreateContact}
-        onUpdateContact={handleUpdateContact}
-        contactToEdit={editingContact}
-      />
-      <ContactList
-        contacts={contacts}
-        onEditContact={handleEditContact}
-        onDeleteContact={handleDeleteContact}
-      />
+      <ContactForm />
     </div>
   );
 };
