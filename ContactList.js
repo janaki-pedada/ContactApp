50038fe0dab05a89d2e0fad09398f41c00050729
janaki-pedada <137858245+janaki-pedada@users.noreply.github.com>
2023-07-29 12:44:35 +0530

    Update ContactList.js

--- ContactList.js
+++ ContactList.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import ContactItem from './ContactItem ';
 
-const ContactList = ({ contacts, onEditContact, onDeleteContact }) => {
+const ContactList = ({ contacts, onEdit, onDelete }) => {
   return (
     <div>
       <h2>Contact List</h2>
       <ul>
-        {contacts.map((contact) => (
-          <li key={contact.id}>
-            {contact.firstName} {contact.lastName} ({contact.phoneNumber}, {contact.email})
-            <button onClick={() => onEditContact(contact)}>Edit</button>
-            <button onClick={() => onDeleteContact(contact)}>Delete</button>
-          </li>
+        {contacts.map((contact, index) => (
+          <ContactItem
+            key={index}
+            contact={contact}
+            onEdit={() => onEdit(index)}
+            onDelete={() => onDelete(index)}
+          />
         ))}
       </ul>
     </div>
